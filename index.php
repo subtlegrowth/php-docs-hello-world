a3e9bfc195a3e84c902b8b879e98450ff0f9517e
subtlegrowth <104740345+subtlegrowth@users.noreply.github.com>
2022-10-04 20:55:54 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
-<html>
-<body>
- 
-<?php
-// Subdomain takeover by subtlegrowth //
-
-# Subdomain takeover by subtlegrowth
-?>
- 
-</body>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>POC</title>
+  </head>
+  <body>
+    <h1><!--Subdomain takeover by subtlegrowth (sam baxter) --></h1>
+  </body>
 </html>
